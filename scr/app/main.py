3f69a015d7da30a9d880ba367398a7cc1900bf5b
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-12-16 22:47:07 -0300

    Update main.py

--- scr/app/main.py
+++ scr/app/main.py
@@ -1,9 +1,19 @@
 #Código teste OTA
 
+#Código teste OTA
+
 from ota_updater import OTAUpdater
 from time import sleep
+import network
 
 def download_and_install_update_if_available():
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('ota', 'otateste')
+    while not sta_if.isconnected():
+        pass
+
+    sleep(1)
     o = OTAUpdater('https://github.com/rdehuyss/chicken-shed-mgr', github_src_dir='scr', main_dir='app')
     o.install_update_if_available_after_boot('ota', 'otateste')
 
@@ -18,7 +28,8 @@ def boot():
     start()
 
 print("Ligando")
-sleep(3)
+sleep(0.5)
 boot()
 print("Feito")
+        
         
