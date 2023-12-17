1284b851eb66ba2f5d13b791e1da69fa641cdda2
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-12-16 22:19:08 -0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,24 @@
+#Código teste OTA
+
+from ota_updater import OTAUpdater
+from time import sleep
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/IanAguiar-ai/teste_ota', github_src_dir = 'src', main_dir = 'app')
+    o.install_update_if_available_after_boot('claro_2g77f524', 'a1969b1974')
+
+
+def start():
+    from aplicacao import main_principal
+    main_principal()
+
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+print("Ligando")
+sleep(1)
+boot()
+print("Feito")
+        
