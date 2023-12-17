601bc81adedd001f4bff8420fc23e94440de0250
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-12-16 22:43:32 -0300

    Create main.py

---
+++ scr/app/main.py
@@ -0,0 +1,24 @@
+#Código teste OTA
+
+from ota_updater import OTAUpdater
+from time import sleep
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/rdehuyss/chicken-shed-mgr', github_src_dir='scr', main_dir='app')
+    o.install_update_if_available_after_boot('ota', 'otateste')
+
+
+def start():
+    from aplicacao import main_principal
+    main_principal()
+
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+print("Ligando")
+sleep(3)
+boot()
+print("Feito")
+        
