9f73bdab90d2f6e5bacca43ef1a8069afd488392
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-12-16 21:59:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 #Código teste OTA
 
-from ota_update.main.ota_updater import OTAUpdater
+from ota_updater import OTAUpdater
+from time import sleep
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/IanAguiar-ai/teste_ota')
+    o.install_update_if_available_after_boot('ota', 'otateste')
 
 
- def download_and_install_update_if_available():
-     o = OTAUpdater('https://github.com/IanAguiar-ai/teste_ota')
-     o.install_update_if_available_after_boot('ota', 'otateste')
+def start():
+    from aplicacao import main_principal
+    main_principal()
 
 
- def start():
-     from aplicacao import main_principal
-     main_principal()
+def boot():
+    download_and_install_update_if_available()
+    start()
 
-
- def boot():
-     download_and_install_update_if_available()
-     start()
-
-
- boot()
+print("Ligando")
+sleep(1)
+boot()
+print("Feito")
